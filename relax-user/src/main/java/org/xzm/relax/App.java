1eec14e94a978bf9633a248923abb587ff785743
bigmother <794720383@qq.com>
2019-03-21 23:28:54 +0800

    增加pid生成器 增加id生成工具类

--- relax-user/src/main/java/org/xzm/relax/App.java
+++ relax-user/src/main/java/org/xzm/relax/App.java
@@ -2,11 +2,14 @@ package org.xzm.relax;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class,args);
+        SpringApplication app = new SpringApplication(App.class);
+        app.addListeners(new ApplicationPidFileWriter());
+        app.run(args);
     }
 }
---
+++ relax-user/src/main/java/org/xzm/relax/util/IdUtils.java
@@ -0,0 +1,11 @@
+package org.xzm.relax.util;
+
+/**
+ * id生成器 snowflake算法
+ */
+public class IdUtils {
+
+    public static long id(){
+        return 0;
+    }
+}
